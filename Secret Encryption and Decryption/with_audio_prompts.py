59c09e69280517414652ff30e5e34335ddba0d03
Sambit Kumar <sambit.kumar142@gmail.com>
2020-09-26 14:17:21 +0530

    added file having same functionality with audio prompt and bug fixes in old file

--- Secret Encryption and Decryption/with_audio_prompts.py
+++ Secret Encryption and Decryption/with_audio_prompts.py
@@ -4,6 +4,21 @@ import base64
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import pyttsx3
+
+
+engine = pyttsx3.init()
+
+def init_speak_engine():
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[10].id)
+    # volume = engine.getProperty('volume')
+    engine.setProperty('volume',1.0)
+    # rate = engine.getProperty('rate')
+    engine.setProperty('rate', 200)
+def speak_msg(msg):
+    engine.say(msg)
+    engine.runAndWait()
 
 class SecretTxt:
     def __init__(self, password):
@@ -49,10 +64,10 @@ class SecretTxt:
                 content = content.encode()
                 encrypted = f.encrypt(content).decode()
             else:
-                return False                
+                return False
+                
         with open(self.file, 'w') as fle:
             fle.write(encrypted+"\n")
-        print("Saved Successfully!\n")
         return True
 
     def view_secret_file(self):
@@ -68,28 +83,57 @@ class SecretTxt:
 
 
 if __name__ == "__main__":
-    print("\nPlease Provide your password.")
-    print("If not created till now, provide the password you want to use or else use your old password")
-    print("Note: If you loose your password then you will not be able to retrive the previous content again.\n")
+    init_speak_engine()
+    msg = "Please Provide your password."
+    print(f"\n{msg}")
+    speak_msg(msg)
+    msg = "If not created till now, provide the password you want to use, or else use your old password"
+    print(msg)
+    speak_msg(msg)
+    msg = "Note: If you loose your password, then you will not be able to retrive the previous content again."
+    print(f"{msg}\n")
+    speak_msg(msg)
+    speak_msg("Please Provide your password")
     password = input("\nProvide your password: ")
+    
     secret = SecretTxt(password)
-    print('\nChoose "r" or "w" to read the secret file or write into the secret file.\n')
+    msg = 'Choose "r", or "w" to read the secret file, or write into the secret file.'
+    print(f'\n{msg}\n')
+    speak_msg(msg)
     again = 'y'
     while again.lower() == 'y':
+        speak_msg("Provide your choice, r to Read, and w to Write.")
         choice = input("\nYour choice (r/w): ")
         if choice.lower() == "r":
+            speak_msg("You've Selected to read. Content will not be read aloud!")
             print(secret.view_secret_file())
         elif choice.lower() == "w":
+            speak_msg("You've Selected to write. If your password would be wrong, we won't be able to write.")
+            speak_msg("Provide yout text to be written into the secret file")
             txt = input("\nProvide your text: ")
-            secret.write_secret_txt(txt)
+            success = secret.write_secret_txt(txt)
+            if success:
+                msg = "Saved Successfully!"
+                print(f"\n{msg}\n")
+                speak_msg(msg)
+            else:
+                msg = "You've provided and incorrect password!"
+                print(f"\n{msg}\n")
+                speak_msg(msg)
         else:
-            print("\nWrong Input! Please try again.\n")
+            msg = "Wrong Input! Please try again."
+            print(f"\n{msg}\n")
+            speak_msg(msg)
         
         for i in range(3):
+            speak_msg("Wanna continue?")
+            speak_msg("Press Y to continue or any other key to exit")
             again = input("\nTry again (y/n): ")
             if again.lower() not in ['y', 'n']:
                 print("\nWrong Input!")
+                speak_msg("Wrong Input. Please Try again.")
             else:
                 break
-        
-    print("Thank you and Bye Byeee!!!")
+    msg = "Thank you and Bye bye!"
+    speak_msg(msg)
+    print(f"\n{msg}\n")
